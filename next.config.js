54ec4e1992c176e8f5552532cdce6491b52988aa
Solace <monkdev9@gmail.com>
2025-11-08 05:41:41 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,6 +13,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/@:username',
+        destination: '/:username',
+      },
+    ];
+  },
   env: {
     RENDER_PUBLIC_URL: process.env.RENDER_PUBLIC_URL,
   },
